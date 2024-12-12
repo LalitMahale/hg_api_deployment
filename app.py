8e4220527ab4fae47eeb4968a7d8ae0f82d49ad6
LalitMahale <lalitmahale121@gmail.com>
2024-12-12 10:00:40 +0530

    modified api

--- app.py
+++ app.py
@@ -3,21 +3,25 @@ from deep_translator import GoogleTranslator
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods = ["GET"])
 def home():
     return jsonify({"message": "Welcome to my Flask API on Hugging Face Spaces!"})
 
-@app.route('/translate')
+@app.route('/translate', methods=['POST'])
 def translate():
-    # Get the 'text' parameter from the query string
-    text = request.args.get('text', '')
+    # Get the 'text' parameter from the JSON body
+    data = request.get_json()
+    text = data.get('text', '')
+    
     if not text:
         return jsonify({"error": "No text provided"}), 400
     
     # Perform translation
     translator = GoogleTranslator(source="auto", target="mr")
     result = translator.translate(text)
+    
     return jsonify({"result": result})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7860)
